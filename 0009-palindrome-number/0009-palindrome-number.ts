0194d68ff9dddbdd003230bbb855fc491dcdad0e
Mihai Hiticas <hiticasmihai7@gmail.com>
2024-05-20 21:00:22 +0200

    Time: 118 ms (93.68%), Space: 59 MB (53.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -0,0 +1,10 @@
+function isPalindrome(x: number): boolean {
+    if (x < 0) {
+        return false;
+    }
+
+    const str = x.toString();
+    const reversedStr = str.split('').reverse().join('');
+
+    return str === reversedStr;
+}
